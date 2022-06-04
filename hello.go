502fc54810effbb9f785a8da7de051293c04066a
Oren Golan <orengolan@gmail.com>
2022-06-04 10:45:50 -0700

    traverse and convert

--- hello.go
+++ hello.go
@@ -15,24 +15,31 @@ func check(e error) {
 	}
 }
 
-func convertFile() {
-	// convert README.md to index.html
-	dat, err := os.ReadFile("README.md")
+// convert README.md to index.html
+func convertFile(path string) {
+	fmt.Printf("inside: %q\n", path)
+	dat, err := os.ReadFile(filepath.Join(path, "README.md"))
 	check(err)
 	output := github_flavored_markdown.Markdown(dat)
-	err = os.WriteFile("index.html", output, 0644)
+	err = os.WriteFile(filepath.Join(path, "index.html"), output, 0644)
 	check(err)
 }
 
 func main() {
+
 	err := filepath.Walk(".", func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
 			return err
 		}
+
+		if info.IsDir() && info.Name() == ".git" {
+			return filepath.SkipDir
+		}
+
 		if info.IsDir() {
 			fmt.Printf("visited file or dir: %q\n", path)
-			/// convertFile(path)
+			convertFile(path)
 			return nil
 		}
 		return nil
